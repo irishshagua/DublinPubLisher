9bddc8301250a3581ef19169b4534c1a20bc2a18
Brian Mooney <brian_satchwannabe@hotmail.com>
2020-11-04 23:14:13 +0000

    See what happens with github api

--- src/Code.js
+++ src/Code.js
@@ -6,5 +6,12 @@ function doPost(e) {
     template.longitude = e.parameters.longitude;
     template.latitude = e.parameters.latitude;
     
+    addPubToList();
+    
     return template.evaluate().setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+
+function addPubToList() {
+    var client = GithubClient('irishagua', 'dublin-pubs-map', 'irishshagua', 'pwd');
+    client.commit('content', 'postgres-pubs-dump.txt', 'brian_satchwannabe@hotmail.com');
 }
