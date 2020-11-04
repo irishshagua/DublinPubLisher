c40f3c74b185d81e5097633415d354af0c58199b
Brian Mooney <brian_satchwannabe@hotmail.com>
2020-11-04 00:03:34 +0000

    add form and allow embedding in iframe

--- src/Code.js
+++ src/Code.js
@@ -1,4 +1,11 @@
-function doPost(e) {
-  var params = JSON.stringify(e);
-  return HtmlService.createHtmlOutput(params);
+function doGet(e) {  
+  var output = HtmlService.createTemplateFromFile('form');
+  return output
+       .evaluate()
+       .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+       .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+
+function doPost(e) {  
+  return HtmlService.createHtmlOutput("This is the post response: " + JSON.stringify(e));
 }
