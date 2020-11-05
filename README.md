5530ed56d59ff23074a5d987a952d962ee7419dc
Brian Mooney <brian_satchwannabe@hotmail.com>
2020-11-05 22:09:00 +0000

    Smudge into one file. Fuck JS. I've no idea what I'm doing

--- README.md
+++ README.md
@@ -11,5 +11,3 @@ This repo is a work in progress. Below is still left to do...
 # Deploy
 There's a github actions pipeline setup to publish the code from [src](src/) to Google Script. It runs the [deployment script](deploy/publish.sh) to deploy with [clasp](https://github.com/google/clasp).
 
-# Attributions
-The GitHubActions file has been borrowed from a [@pamelafox gist](https://gist.github.com/pamelafox/ea0474daa137f035b489bf78cc5797ea), so much appreciation there.
--- src/Code.js
+++ src/Code.js
@@ -1,3 +1,6 @@
+const TOKEN = 'abc123';
+const GITHUB_URL = "https://api.github.com/repos/irishshagua/dublin-pubs-map/contents/postgres-pubs-dump.txt";
+
 function doPost(e) {  
     var template = HtmlService.createTemplateFromFile('response');
   
@@ -9,4 +12,59 @@ function doPost(e) {
     addNewPub(template.pubName, template.review, template.latitude, template.longitude);
     
     return template.evaluate().setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+}
+ 
+function addNewPub(pubName, pubReview, latitude, longitude) {
+    Logger.log('Attempting to add [' + pubName + ']');
+    
+    // Get current content
+    var result = retrieveContent();
+    var sha = result[0];
+    var content = result[1];
+    
+    // Add new pub
+    var updatedContent = content + "\n" + latitude + "\t" + longitude + "\t" + pubName + "\t" + pubReview;
+    
+    // Update content
+    var result = updateContent(updatedContent, sha);
+};
+
+function retrieveContent() {
+    var headers = {
+        "Authorization": "Bearer " + TOKEN,
+        "Accept": "application/vnd.github.v3+json"
+    };
+  
+    var options = {'headers': headers, method: 'GET'};
+  
+    var resp = UrlFetchApp.fetch(GITHUB_URL, options);
+    Logger.log('Retrieving content from GitHub gave ' + resp);
+    
+    var json = JSON.parse(resp);
+    var sha = json.sha;
+    var content = Utilities.base64Decode(json.content);
+    
+    return [sha, content];
+}
+
+function updateContent(updatedContent, previousSha) {
+    var headers = {
+        "Authorization": "Bearer " + TOKEN,
+        "Accept": "application/vnd.github.v3+json"
+    };
+  
+    var options = {
+        headers: headers, 
+        method: 'PUT',
+        contentType: 'application/json',
+        payload: JSON.stringify({
+            message: '[Google App Bot] Adding a new pub',
+            sha: previousSha,
+            content: Utilities.base64Decode(json);
+        })
+    };
+  
+    var resp = UrlFetchApp.fetch(GITHUB_URL, options);
+    
+    return JSON.parse(resp);
 }
